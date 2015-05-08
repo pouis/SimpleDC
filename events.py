ce8cfd865a835b74101b99a1d6ab5735426ddcda
pouis <pouis@users.noreply.github.com>
2015-05-08 10:48:09 +0200

    modified names of variables eventnb --> event_nb

--- events.py
+++ events.py
@@ -1,67 +1,67 @@
 
 class Event:
     """common base class for all events"""
-    def __init__(self, eventnb, subeventnb, eventtext, eventtest, eventhurdle,
-                 incidence, incidencemin, incidencemax, expmin, expmax,
-                 goldmin, goldmax, msgeventsucceed, msgeventfail):
-        self.eventnb = eventnb
-        self.subeventnb = subeventnb
-        self.eventtext = eventtext
-        self.eventtest = eventtest
-        self.eventhurdle = eventhurdle
+    def __init__(self, event_nb, subevent_nb, event_text, event_test, event_hurdle,
+                 incidence, incidence_min, incidence_max, exp_min, exp_max,
+                 gold_min, gold_max, msg_event_succeed, msg_event_fail):
+        self.event_nb = event_nb
+        self.subevent_nb = subevent_nb
+        self.event_text = event_text
+        self.event_test = event_test
+        self.event_hurdle = event_hurdle
         self.incidence = incidence
-        self.incidencemin = incidencemin
-        self.incidencemax = incidencemax
-        self.expmin = expmin
-        self.expmax = expmax
-        self.goldmin = goldmin
-        self.goldmax = goldmax
-        self.msgeventsucceed = msgeventsucceed
-        self.msgeventfail = msgeventfail
+        self.incidence_min = incidence_min
+        self.incidence_max = incidence_max
+        self.exp_min = exp_min
+        self.exp_max = exp_max
+        self.gold_min = gold_min
+        self.gold_max = gold_max
+        self.msg_event_succeed = msg_event_succeed
+        self.msg_event_fail = msg_event_fail
 
     def display(self):
-        print(self.eventnb)
+        print(self.event_nb)
 
 
 class Explore(Event):
     def __init__(self):
-        super().__init__(eventnb=0, subeventnb=0, eventtext="探索を続ける", eventtest="_hp", eventhurdle=20,
-                 incidence="_hp", incidencemin=3, incidencemax=7, expmin=0, expmax=0,
-                 goldmin=0, goldmax=2, msgeventsucceed="まだまだいける", msgeventfail="ちょっと疲れた")
+        super().__init__(event_nb=0, subevent_nb=0, event_text="探索を続ける", event_test="_hp", event_hurdle=20,
+                 incidence="_hp", incidence_min=3, incidence_max=7, exp_min=0, exp_max=0,
+                 gold_min=0, gold_max=2, msg_event_succeed="まだまだいける", msg_event_fail="ちょっと疲れた")
 
 # explore = Event(1, 0, "探索を続ける", "Stamina", 6, 20,
 #                "Stamina", 6, 3, 7, 0, 0,
 #                0, 2, "まだまだいける", "ちょっと疲れた")
 
 # explore.display()
-# print(explore.eventtext)
+# print(explore.event_text)
 # a = Explore()
-# print(a.eventtext)
+# print(a.event_text)
 
 
 class Encounter(Event):
     def __init__(self):
-        super().__init__(eventnb=1, subeventnb=0, eventtext="ゾンビに襲われた！", eventtest="_str", eventhurdle=50,
-                 incidence="_hp", incidencemin=3, incidencemax=7, expmin=1, expmax=1,
-                 goldmin=0, goldmax=3, msgeventsucceed="逃げた。", msgeventfail="なんとか撃退した。")
+        super().__init__(event_nb=1, subevent_nb=0, event_text="ゾンビに襲われた！", event_test="_str", event_hurdle=50,
+                 incidence="_hp", incidence_min=3, incidence_max=7, exp_min=1, exp_max=1,
+                 gold_min=0, gold_max=3, msg_event_succeed="逃げた。", msg_event_fail="なんとか撃退した。")
 
 
 class Pit(Event):
     def __init__(self):
-        super().__init__(eventnb=2, subeventnb=0, eventtext="落とし穴だ！", eventtest="_agi", eventhurdle=60,
-                 incidence="_hp", incidencemin=3, incidencemax=7, expmin=1, expmax=2,
-                 goldmin=0, goldmax=4, msgeventsucceed="切り抜けた。", msgeventfail="落ちた・・・")
+        super().__init__(event_nb=2, subevent_nb=0, event_text="落とし穴だ！", event_test="_agi", event_hurdle=60,
+                 incidence="_hp", incidence_min=3, incidence_max=7, exp_min=1, exp_max=2,
+                 gold_min=0, gold_max=4, msg_event_succeed="切り抜けた。", msg_event_fail="落ちた・・・")
 
 
 class HiddenDoor(Event):
     def __init__(self):
-        super().__init__(eventnb=3, subeventnb=0, eventtext="気配を感じる！", eventtest="_int", eventhurdle=60,
-                 incidence="_hp", incidencemin=3, incidencemax=7, expmin=1, expmax=2,
-                 goldmin=0, goldmax=4, msgeventsucceed="隠し扉を発見！", msgeventfail="何もなかった・・・")
+        super().__init__(event_nb=3, subevent_nb=0, event_text="気配を感じる！", event_test="_int", event_hurdle=60,
+                 incidence="_hp", incidence_min=3, incidence_max=7, exp_min=1, exp_max=2,
+                 gold_min=0, gold_max=4, msg_event_succeed="隠し扉を発見！", msg_event_fail="何もなかった・・・")
 
 
 class Treasure(Event):
     def __init__(self):
-        super().__init__(eventnb=4, subeventnb=0, eventtext="宝箱を発見！", eventtest="_agi", eventhurdle=60,
-                 incidence="_hp", incidencemin=3, incidencemax=7, expmin=1, expmax=2,
-                 goldmin=0, goldmax=4, msgeventsucceed="色々手に入れた！", msgeventfail="開かなかった・・・")
+        super().__init__(event_nb=4, subevent_nb=0, event_text="宝箱を発見！", event_test="_agi", event_hurdle=60,
+                 incidence="_hp", incidence_min=3, incidence_max=7, exp_min=1, exp_max=2,
+                 gold_min=0, gold_max=4, msg_event_succeed="色々手に入れた！", msg_event_fail="開かなかった・・・")
